c105afe5ac91bc54296ae22d204887544475f5b9
Yslas <jryslas@gmail.com>
2019-01-14 18:14:06 -0800

    Updates

---
+++ precache-manifest.6c1ec6811085754e49afd2fe85a2f0de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96b775dea8b83ebc2f0",
+    "url": "/ReactHW/static/js/runtime~main.e96b775d.js"
+  },
+  {
+    "revision": "0f932cedeae3eb589d0b",
+    "url": "/ReactHW/static/js/main.0f932ced.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ReactHW/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "0f932cedeae3eb589d0b",
+    "url": "/ReactHW/static/css/main.ca129c4f.chunk.css"
+  },
+  {
+    "revision": "c574c7360290bd89c4932ebf30884024",
+    "url": "/ReactHW/index.html"
+  }
+];
